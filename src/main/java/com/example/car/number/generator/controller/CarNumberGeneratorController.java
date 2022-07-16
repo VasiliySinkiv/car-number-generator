96047f734bdacec4948738d910d8cb692cb0d16f
Василий Синькив <vasili.sinkiv@yandex.ru>
2022-07-16 20:03:45 +0600

    commit

--- src/main/java/com/example/car/number/generator/controller/CarNumberGeneratorController.java
+++ src/main/java/com/example/car/number/generator/controller/CarNumberGeneratorController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/number")
 public class CarNumberGeneratorController {
 
+
     @Autowired
     private CarNumberGeneratorService numberGeneratorService;
 
